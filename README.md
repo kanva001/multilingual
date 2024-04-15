0a7af177d903b3c6a8c53ca7db63e544adb13e48
Vasanth <kanva001@gmail.com>
2024-04-15 15:55:01 -0400

    folder structure created

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# multilingual
+# Multilingual Assistant
+
+# How to run
+
+conda create -n multilingual python=3.8 -y
+
+conda activate multilingual
--- template.py
+++ template.py
@@ -2,3 +2,32 @@ import os
 import logging
 from pathlib import Path
 
+logging.basicConfig(level=logging.INFO, format='[%(activate)s]:%(message)s:')
+
+# Creating folder architecture
+
+list_of_files = [
+    
+    "src/__init__.py",
+    "src/helper.py",
+    ".env",
+    "requirements.txt",
+    "setup.py",
+    "app.py",
+    "research/trails.ipynb"
+    
+]
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"creating directory {filedir} for the files: {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) ==0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"creating empty file: {filepath}")
+    else:
+        print(f"file is already present at: {filepath}")
+        logging.info (f"{filename} already exists")
