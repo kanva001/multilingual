786e147d3edbfe314116066d4e8909a275b79534
Vasanth <kanva001@gmail.com>
2024-04-15 17:32:54 -0400

    added files

--- src/helper.py
+++ src/helper.py
@@ -5,3 +5,21 @@ import os
 from gtts import gTTS
 
 print("perfect !")
+
+load_dotenv()
+
+GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+os.environ["GOOGLE_API_KEY"]=GOOGLE_API_KEY
+
+
+# code fot voice assitnat
+
+def voice_input():
+    pass
+
+def text_to_speech():
+    pass
+
+def llm_model_object():
+    pass
+
