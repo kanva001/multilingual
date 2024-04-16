a109f1f18ef08c6674461f105193ba7cf5cc0b27
Vasanth <kanva001@gmail.com>
2024-04-15 20:05:53 -0400

    updated app.py and helper.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from src.helper import voice_input, llm_model_object, text_to_speech
 
+
 def main():
-    st.title("Multilingual AI Assistant !!")
+    st.title("Multilingual AI Assistant 🤖")
     
     if st.button("Ask me anything"):
         with st.spinner("Listening..."):
@@ -10,13 +11,17 @@ def main():
             response=llm_model_object(text)
             text_to_speech(response)
             
-            audio_file=open("speech.mp3", "rb")
+            
+            audio_file=open("speech.mp3","rb")
             audio_bytes=audio_file.read()
             
-            st.text_area()
-            st.audio()
-            st.download_button()
             
-
-if__name__=='__main__':
+            st.text_area(label="Response:", value=response, height=350)
+            st.audio(audio_bytes)
+            st.download_button(label="Download Speech",
+                               data=audio_bytes,
+                               file_name="speech.mp3",
+                               mime="audio/mp3")
+            
+if __name__=='__main__':
     main()
--- src/helper.py
+++ src/helper.py
@@ -15,11 +15,34 @@ os.environ["GOOGLE_API_KEY"]=GOOGLE_API_KEY
 # code fot voice assitnat
 
 def voice_input():
-    pass
+    r=sr.Recognizer()
+    
+    with sr.Microphone() as source:
+        print("Listening..")
+        audio=r.listen(source)
+    try:
+        text=r.recognize_google(audio)
+        print("You said: ", text)
+        return text
+    except sr.UnknownValueError:
+        print("Sorry, could not understand the audio")
+    except sr.RequestError as e:
+        print("Could not request result from Speech Recognition service: {0}".format(e))
+    
 
-def text_to_speech():
-    pass
+def text_to_speech(text):
+    tts=gTTS(text=text, lang='en')
+    
+    # Save the speech from the given test in the .mp3 format
+    tts.save("speech.mp3")
+    
 
-def llm_model_object():
-    pass
+def llm_model_object(user_text):
+    model="model/gemini-pro"
+    
+    model = genai.GenerativeModel(model)
+    response = model.generate_content(user_text)
+    result = response.text
+    
+    return result
 
